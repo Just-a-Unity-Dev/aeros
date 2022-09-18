56d909efdd88a5245815842145bb8775e300fef3
Just-a-Unity-Dev <just-a-unity-dev@users.noreply.github.com>
2022-09-18 20:58:40 +0800

    FOV

--- src/main.rs
+++ src/main.rs
@@ -3,29 +3,49 @@ use tcod::colors::*;
 use tcod::input::Key;
 use tcod::input::KeyCode::*;
 use tcod::BackgroundFlag::*;
+use tcod::map::{FovAlgorithm, Map as FovMap};
 
 mod maps;
 mod tile;
 
+// rendering
 const SCREEN_WIDTH: i32 = 80;
 const SCREEN_HEIGHT: i32 = 50;
 const LIMIT_FPS: i32 = 60;
 
+// map stuff
 const MAP_WIDTH: i32 = 80;
 const MAP_HEIGHT: i32 = 45;
 
+// FOV
+const FOV_ALGO: FovAlgorithm = FovAlgorithm::Basic; // default FOV algorithm
+const FOV_LIGHT_WALLS: bool = true; // light walls or not
+const TORCH_RADIUS: i32 = 3;
+
+// colors
 const COLOR_DARK_WALL: Color = Color { r: 30, g: 30, b: 30 };
+const COLOR_LIGHT_WALL: Color = Color {
+    r: 90,
+    g: 90,
+    b: 90,
+};
 const COLOR_DARK_GROUND: Color = Color {
-    r: 0,
-    g: 0,
-    b: 0,
+    r: 10,
+    g: 10,
+    b: 10,
+};
+const COLOR_LIGHT_GROUND: Color = Color {
+    r: 180,
+    g: 180,
+    b: 180,
 };
 
 // structs
 
 struct Tcod {
     root: Root,
-    con: Offscreen
+    con: Offscreen,
+    fov: FovMap
 }
 
 struct Game {
@@ -59,25 +79,39 @@ impl Object {
     }
 }
 
-fn render_all(tcod: &mut Tcod, game: &Game, objects: &[Object]) {
+fn render_all(tcod: &mut Tcod, game: &Game, objects: &[Object], fov_recompute: bool) {
     // draw all objects in the list
     for object in objects {
-        object.draw(&mut tcod.con);
+        if tcod.fov.is_in_fov(object.x, object.y) {
+            object.draw(&mut tcod.con);
+        }
+    }
+    
+    if fov_recompute {
+        // recompute FOV if needed (the player moved or something)
+        let player = &objects[0];
+        tcod.fov
+            .compute_fov(player.x, player.y, TORCH_RADIUS, FOV_LIGHT_WALLS, FOV_ALGO);
     }
 
     // go through all tiles, and set their background color
-    for y in 0..MAP_HEIGHT {
-        for x in 0..MAP_WIDTH {
-            let wall = game.map[x as usize][y as usize].block_sight;
-            if wall {
-                tcod.con
-                    .set_char_background(x, y, COLOR_DARK_WALL, BackgroundFlag::Set);
-            } else {
-                tcod.con
-                    .set_char_background(x, y, COLOR_DARK_GROUND, BackgroundFlag::Set);
-            }
-        }
+    // go through all tiles, and set their background color
+for y in 0..MAP_HEIGHT {
+    for x in 0..MAP_WIDTH {
+        let visible = tcod.fov.is_in_fov(x, y);
+        let wall = game.map[x as usize][y as usize].block_sight;
+        let color = match (visible, wall) {
+            // outside of field of view:
+            (false, true) => COLOR_DARK_WALL,
+            (false, false) => COLOR_DARK_GROUND,
+            // inside fov:
+            (true, true) => COLOR_LIGHT_WALL,
+            (true, false) => COLOR_LIGHT_GROUND,
+        };
+        tcod.con
+            .set_char_background(x, y, color, BackgroundFlag::Set);
     }
+}
 
     blit(&tcod.con, (0,0), (MAP_WIDTH, MAP_HEIGHT), &mut tcod.root, (0,0), 1.0, 1.0);
 }
@@ -90,15 +124,32 @@ fn main() {
         .title("Aeros")
         .init();
     
-    let player = Object::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, '@',  WHITE);
-    let con = Offscreen::new(MAP_WIDTH, MAP_HEIGHT);
+    let player = Object::new(25, 23, '@',  WHITE);
     let game = Game {
         map: maps::make_map(MAP_HEIGHT, MAP_WIDTH)
     };
     let mut objects = [player];
     
-    let mut tcod = Tcod { root, con };
+    let mut tcod = Tcod { 
+        root, 
+        con: Offscreen::new(MAP_WIDTH, MAP_HEIGHT), 
+        fov: FovMap::new(MAP_WIDTH, MAP_HEIGHT) 
+    };
     tcod::system::set_fps(LIMIT_FPS);
+
+    for y in 0..MAP_HEIGHT {
+        for x in 0..MAP_WIDTH {
+            tcod.fov.set(
+                x,
+                y,
+                !game.map[x as usize][y as usize].block_sight,
+                !game.map[x as usize][y as usize].blocked,
+            );
+        }
+    }
+
+    // force FOV "recompute" first time through the game loop
+    let mut previous_player_position = (-1, -1);
 
     while !tcod.root.window_closed() {
         // refresh the console
@@ -108,10 +159,12 @@ fn main() {
         }
 
         // render it
-        render_all(&mut tcod, &game, &objects);
+        let fov_recompute = previous_player_position != (objects[0].x, objects[0].y);
+        render_all(&mut tcod, &game, &objects, fov_recompute);
         tcod.root.flush();
 
         let player = &mut objects[0];
+        previous_player_position = (player.x, player.y);
         let exit = handle_input(&mut tcod, &game, player);
         if exit {
             break;
--- src/maps.rs
+++ src/maps.rs
@@ -1,10 +1,60 @@
 use crate::tile::Tile;
+use std::cmp;
 
+// maps part
 pub type Map = Vec<Vec<Tile>>;
 
 pub fn make_map(map_height: i32, map_width: i32) -> Map {
-    let mut map = vec![vec![Tile::empty(); map_height as usize]; map_width as usize];
-    map[30][22] = Tile::wall();
-    map[50][22] = Tile::wall();
+    let mut map = vec![vec![Tile::wall(); map_height as usize]; map_width as usize];
+    let room1: Rect = Rect::new(20, 15, 10, 15);
+    let room2: Rect = Rect::new(50, 15, 10, 15);
+    create_room(room1, &mut map);
+    create_room(room2, &mut map);
+    create_h_tunnel(25, 55, 23, &mut map);
+
     map
+}
+
+// room part
+/// A rectangle on the map, used to characterise a room.
+#[derive(Clone, Copy, Debug)]
+pub struct Rect {
+    x1: i32,
+    y1: i32,
+    x2: i32,
+    y2: i32,
+}
+
+impl Rect {
+    pub fn new(x: i32, y: i32, w: i32, h: i32) -> Self {
+        Rect {
+            x1: x,
+            y1: y,
+            x2: x + w,
+            y2: y + h,
+        }
+    }
+}
+
+pub fn create_room(room: Rect, map: &mut Map) {
+    // go through the tiles in the rectangle and make them passable
+    for x in (room.x1 + 1)..room.x2 {
+        for y in (room.y1 + 1)..room.y2 {
+            map[x as usize][y as usize] = Tile::empty();
+        }
+    }
+}
+
+pub fn create_h_tunnel(x1: i32, x2: i32, y: i32, map: &mut Map) {
+    // horizontal tunnel. `min()` and `max()` are used in case `x1 > x2`
+    for x in cmp::min(x1, x2)..(cmp::max(x1, x2) + 1) {
+        map[x as usize][y as usize] = Tile::empty();
+    }
+}
+
+pub fn create_v_tunnel(y1: i32, y2: i32, x: i32, map: &mut Map) {
+    // vertical tunnel
+    for y in cmp::min(y1, y2)..(cmp::max(y1, y2) + 1) {
+        map[x as usize][y as usize] = Tile::empty();
+    }
 }
